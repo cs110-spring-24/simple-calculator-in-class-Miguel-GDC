cfca56de1b26f0c00db9d0cf6fd459aa5237aa41
Miguel-GDC <157658233+Miguel-GDC@users.noreply.github.com>
2024-02-05 16:18:44 -0800

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,1 +1,36 @@
+num 1 = input(input: ("your num 1: "))
+op = input(input: ("your operator: "))
+num 2 = input(input: "your operator: ")
 
+if operator == "+":
+	total = num 1 + num 2
+	print (f"{num 1} + {num 2} = {total}")
+#the "if" statement makes the code which operation to carry out
+
+if operator == "-":
+	total = num 1 - num 2
+	print (f"{num 1} - {num 2} = {total}")
+
+if operator == "*":
+	total = num 1 * num 2
+	print (f"{num 1} * {num 2} = {total}")
+
+if operator == "/":
+	total = num 1 * num 2
+	print ({f"{num 1} / {num 2} = {total}"})
+
+if operator == "**":
+	total = num 1 ** num 2
+	print ({f"{num 1} ** {num 2} = {total}"})
+
+if operator == "//":
+	total = num 1 // num 2
+	print ({f"{num 1} // {num 2} = {total}"})
+
+if operator == "%":
+	total = num 1 ** num 2
+	print ({f"{num 1} % {num 2} = {total}"}) 
+
+else:
+	print ("Invalid operator")
+	print ("Valid operators include: +, -, *, /, **, //, %")
